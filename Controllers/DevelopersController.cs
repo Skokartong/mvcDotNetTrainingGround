eff82886a354ac8d29c802fc2fa386a93166d78e
Amanda Olving <amanda.olving@gmail.com>
2025-10-02 17:04:05 +0200

    Add DevelopersController

---
+++ Controllers/DevelopersController.cs
@@ -0,0 +1,11 @@
+namespace mvcDotNetTrainingGround.Controllers
+{
+    using System.Data.Common;
+    using Microsoft.AspNetCore.Mvc;
+    using mvcDotNetTrainingGround.Models;
+
+    public class DevelopersController : Controller
+    {
+
+    }
+}
